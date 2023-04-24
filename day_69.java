70e83728297ad5100a8aac9d385664ccb8fb4bc4
Saurabh Shinde <123807524+Shindes6642@users.noreply.github.com>
2023-04-24 19:16:45 +0530

    Add files via upload

---
+++ day_69.java
@@ -0,0 +1,29 @@
+class Isosceles{
+    void display1()
+    {
+    System.out.println("I am an isosceles triangle");
+    }
+   }
+   class Equilateral extends Isosceles{
+    void display2()
+    {
+    System.out.println("I am an equilateral triangle");
+    }
+   
+   }
+   class C extends Equilateral{
+    void display3()
+    {
+    System.out.println("I am triangle");
+    }
+   
+   }
+   public class day_69
+   {
+   public static void main(String[] args) {
+   C c = new C();
+   c.display2();
+   c.display1();
+   c.display3();
+   }
+   }
